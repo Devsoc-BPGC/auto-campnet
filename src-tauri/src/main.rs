6099c7e3c4944c7fedf58d711bf4c5b8fdd48e79
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2022-03-16 14:23:37 +0530

    Pass credentials to backend
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,21 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Credentials {
+  username: String,
+  password: String
+}
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      app.listen_global("save", |event| {
+        let creds: Credentials = serde_json::from_str(event.payload().unwrap()).unwrap();
+        println!("Got credentials with username: {} and password: {}", creds.username, creds.password);
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
