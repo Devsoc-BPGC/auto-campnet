df85de9ae2dc1f1acee04de3a0346c98becec995
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2022-03-17 11:49:50 +0530

    Save credentials to file in config directory
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
 use tauri::Manager;
 use serde::{Deserialize, Serialize};
+use std::io::Write;
 
 #[derive(Serialize, Deserialize)]
 struct Credentials {
@@ -7,15 +8,24 @@ struct Credentials {
   password: String
 }
 
+fn save_creds(creds: Credentials, save_file: &std::path::Path){
+  let mut file = std::fs::File::create(&save_file).unwrap();
+  writeln!(&mut file, "{}", serde_json::to_string(&creds).unwrap()).unwrap();
+}
+
+
 fn main() {
   tauri::Builder::default()
-    .setup(|app| {
-      app.listen_global("save", |event| {
-        let creds: Credentials = serde_json::from_str(event.payload().unwrap()).unwrap();
-        println!("Got credentials with username: {} and password: {}", creds.username, creds.password);
-      });
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  .setup(|app: &mut tauri::App| {
+    let save_dir = tauri::api::path::app_dir(&app.config()).unwrap();
+    let save_file = save_dir.join("credentials.json");
+    std::fs::create_dir_all(save_dir).unwrap();
+    app.listen_global("save", move |event: tauri::Event| {
+      let creds: Credentials = serde_json::from_str(event.payload().unwrap()).unwrap();
+      save_creds(creds, &save_file);
+    });
+    Ok(())
+  })
+  .run(tauri::generate_context!())
+  .expect("error while running tauri application");
 }
