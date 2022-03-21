125b37b75035e21fe0d7ccd27605b23ed73c8a00
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2022-03-21 20:28:51 +0530

    Login to campnet based on status of different HTTP requests
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
-use tauri::Manager;
+use tauri::{Manager, api::{path, file}};
 use serde::{Deserialize, Serialize};
 use std::io::Write;
+extern crate timer;
+extern crate chrono;
 
 #[derive(Serialize, Deserialize)]
 struct Credentials {
@@ -14,7 +16,7 @@ fn save_creds(creds: Credentials, save_file: &std::path::Path){
 }
 
 fn load_creds(save_file: &std::path::Path) -> Result<Credentials, String> {
-  let creds_string = tauri::api::file::read_string(save_file);
+  let creds_string = file::read_string(save_file);
   if creds_string.is_ok() {
     let creds: Credentials = serde_json::from_str(&creds_string.unwrap()).unwrap();
     return Ok(creds);
@@ -24,26 +26,80 @@ fn load_creds(save_file: &std::path::Path) -> Result<Credentials, String> {
   }
 }
 
+fn connect_campnet(file_path: &std::path::PathBuf) {
+  let campnet_status = reqwest::blocking::get("https://campnet.bits-goa.ac.in:8090/");
+  if campnet_status.is_ok() {
+    let login_status = reqwest::blocking::get("https://www.google.com");
+    if login_status.is_err() {
+      let helper_file = file_path.parent().unwrap().join("credentials.json");
+      let creds = load_creds(&helper_file);
+      if creds.is_ok() {
+        let creds = creds.unwrap();
+        let body: String = format!("mode=191&username={}&password={}&a={}&producttype=1", creds.username, creds.password, std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_millis());
+        let client = reqwest::blocking::Client::new();
+        let res = client.post("https://campnet.bits-goa.ac.in:8090/login.xml")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Content-Length", body.chars().count())
+            .body(body)
+            .send();
+        if res.is_ok() {
+          let res_body: String = res.unwrap().text().unwrap();
+          if res_body.contains("LIVE") {
+            tauri::api::notification::Notification::new("com.riskycase.autocampnet")
+              .title("Connected to Campnet!")
+              .body("Logged in successfully to BPGC network")
+              .show();
+            println!("Conn succ");
+          }
+          else {
+            tauri::api::notification::Notification::new("com.riskycase.autocampnet")
+              .title("Could not to Campnet!")
+              .body("There was an issue with the login attempt")
+              .show();
+            println!("Conn issue");
+          }
+        }
+      }
+    }
+    else {
+      println!("loggeed in already");
+    }
+  }
+  else {
+    println!("Not campnet");
+  }
+
+  let callback_timer = timer::Timer::new();
+  let callback_path = file_path.parent().unwrap().join("credentials.json");
+  let _callback_gaurd = callback_timer.schedule_with_delay(chrono::Duration::milliseconds(2500), move || {
+    connect_campnet(&callback_path);
+  });
+  std::thread::sleep(std::time::Duration::from_millis(3000));
+}
+
 fn main() {
   tauri::Builder::default()
   .setup(|app: &mut tauri::App| {
-    let save_dir = tauri::api::path::app_dir(&app.config()).unwrap();
-    let save_file = save_dir.join("credentials.json");
+    let save_dir = path::app_dir(&app.config()).unwrap();
     let file_creds = load_creds(&(save_dir.join("credentials.json")));
     if file_creds.is_ok() {
-      println!("Credentials present");
-      let creds = file_creds.unwrap();
-      println!("Username: {}", creds.username);
-      println!("Password: {}", creds.password);
+      let _creds = file_creds.unwrap();
     }
     else {
       println!("Credentials absent");
     }
-    std::fs::create_dir_all(save_dir).unwrap();
+    let write_save_file = save_dir.join("credentials.json");
     app.listen_global("save", move |event: tauri::Event| {
       let creds: Credentials = serde_json::from_str(event.payload().unwrap()).unwrap();
-      save_creds(creds, &save_file);
+      save_creds(creds, &write_save_file);
     });
+    let callback_timer = timer::Timer::new();
+    let read_save_file = save_dir.join("credentials.json");
+    let _callback_gaurd = callback_timer.schedule_with_delay(chrono::Duration::milliseconds(0), move || {
+      connect_campnet(&read_save_file);
+    });
+    std::thread::sleep(std::time::Duration::from_micros(100));
+    std::fs::create_dir_all(save_dir).unwrap();
     Ok(())
   })
   .run(tauri::generate_context!())
