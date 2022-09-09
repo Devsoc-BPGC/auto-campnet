90781b61f7286b474d699e50b852b88dfb4954f8
ruchikxxii <ruchik710@gmail.com>
2022-09-09 19:06:46 +0530

    added hover animation for button and images and logos of pilani and hyderabad campus

--- ui/index.js
+++ ui/index.js
@@ -30,3 +30,21 @@ listen("credentials", (creds) => {
 document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "hidden") emit("minimise");
 });
+
+//set using backend
+const campus = "hyderabad";
+
+const campusImages = {
+    goa:"https://180dc.org/wp-content/uploads/2016/04/bits-goa-campus.jpg",
+    pilani:"https://www.bits-pilani.ac.in/Uploads/Pilani/pilanimanagementadmin/Gallery/2019-3-3--0-59-3-979_22158613_310339669446805_463247969886404608_n.jpg",
+    hyderabad:"https://www.bits-pilani.ac.in/Uploads/Hyderabad/adminforhyderabad/Gallery/2015-6-25--15-1-11-412_audi1.JPG"
+}; 
+
+const campusLogos = {
+    goa:"https://www.bits-pilani.ac.in/Uploads/Campus/BITS_Goa_campus_logo.gif",
+    pilani:"https://www.bits-pilani.ac.in/Uploads/Campus/BITS_Pilani_campus_logo.gif",
+    hyderabad:"https://www.bits-pilani.ac.in/Uploads/Campus/BITS_Hyderabad_campus_logo.gif"
+}; 
+
+document.body.style.backgroundImage = `url(${campusImages[campus]})`;
+document.querySelector(".logo").setAttribute("src",campusLogos[campus]);
